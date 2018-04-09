8a6a62caabd16fe15aa2815f84e407062dee2f34
AaronTheDesigner <flux.charlotte@gmail.com>
2018-04-09 18:04:14 -0400

    complete

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -8,10 +8,39 @@ import React, { Component } from 'react';
 // including {Component} in import statement does the same thing.
 
 class SearchBar extends Component {
+    //the following constructor initializes the state
+    constructor(props) {
+        super(props);
+        //this.state = {} creates a new object to add properties
+        this.state = { term: '' };
+    }
+
     render() {
-        return <input/>;
+        //this refers to anything within SearchBar's render
+        return (
+        <div className = "search-bar">
+            <input 
+            value = {this.state.term}
+            onChange = { event => this.onInputChange(event.target.value)} />
+            
+        </div>
+        );
+    }
+
+    //event handler, named 'handler'/'on'
+    /*onInputChange(event) {
+        console.log(event);
+    }*/
+    onInputChange(term) {
+        this.setState({term});
+        this.props.onSearchTermChange(term);
     }
 
 }
 
+/* Understanding State
+State is a plain javascript object used to record and REACT to user based events.
+Each class based component has its own state object
+whenever the object is changed it will be rerendered along with it's children.
+*/
 export default SearchBar;
